77266b6e2bc9280c8a4f9870e4d7997acc96b872
Arnau Villoro <arnau@villoro.com>
2025-02-25 18:16:42 +0100

    Simplify code

--- vdropbox/vdropbox.py
+++ vdropbox/vdropbox.py
@@ -1,310 +1,130 @@
 import io
+from pathlib import Path
 from zipfile import ZipFile
 
 import dropbox
 import oyaml as yaml
 import pandas as pd
 from dropbox.files import WriteMode
-
-
-class DummyLog:
-    """Dummy log that prints"""
-
-    def info(self, msg):
-        print(msg)
-
-    def warning(self, msg):
-        print(msg)
-
-    def error(self, msg):
-        print(msg)
+from loguru import logger
 
 
 class Vdropbox:
-    """Vdropbox object"""
+    """Dropbox client for handling files, structured data, and compressed archives."""
 
-    def __init__(self, token, log=DummyLog()):
-        """
-        Creates the vdropbox object
-
-        Args:
-            token:  for connection to dropbox
-            log:    log object. The default one only prints
-        """
-
+    def __init__(self, token, logger=logger):
+        """Initialize the Dropbox client."""
         self.dbx = dropbox.Dropbox(token)
-        self.log = log
-
-    def _check_one_file(self, path, filename):
-        """
-        Internal function to check if a file exists in dropbox
-
-        Args:
-            path:       folder of the file
-            filename:   name of the file
-        """
-
-        for match in self.dbx.files_search(path, filename).matches:
-            if filename == match.metadata.name:
-                return True
-
-        return False
-
-    def _check_path(self, path):
-        """Check that a path starts with '/'"""
-
-        if not path.startswith("/"):
-            path = "/" + path
-
-        return path
+        self.logger = logger
 
     def file_exists(self, uri):
-        """
-        Check if a file exists in dropbox
+        """Check if a file exists in Dropbox."""
+        path = Path(uri)
+        folder, filename = str(path.parent), path.name
 
-        Args:
-            uri:    file uri
-        """
-
-        # Check all folders before the actual file
-        data = uri.split("/")
-        for i, filename in enumerate(data):
-            path = "/".join(data[:i])
-
-            # Skip if filename is empty (happens when uri starts with '/')
-            if not filename:
-                continue
-
-            # All non empty path must start with '/'
-            if path and not path.startswith("/"):
-                path = "/" + path
-
-            # If one file or folder don't exist return False
-            if not self._check_one_file(path, filename):
-                return False
-
-        # If all exists return true
-        return True
+        for match in self.dbx.files_search(folder, filename).matches:
+            if filename == match.metadata.name:
+                return True
+        return False
 
     def ls(self, folder):
-        """List entries in a folder"""
-
-        folder = self._check_path(folder)
-
-        return sorted([x.name for x in self.dbx.files_list_folder(folder).entries])
-
-    def mv(self, origin, destination, overwrite=True):
-        """Move a file by copying and deleting"""
-
-        self.log.debug(f"Moving file '{origin}' to '{destination}'")
-
-        if overwrite and self.file_exists(destination):
-            self.dbx.files_delete(destination)
-
-        self.dbx.files_copy(origin, destination)
-        self.dbx.files_delete(origin)
+        """List files in a folder."""
+        folder = str(Path(folder).resolve())
+        return sorted(
+            entry.name for entry in self.dbx.files_list_folder(folder).entries
+        )
 
     def delete(self, filename):
-        """Delete a file/folder from dropbox"""
+        """Delete a file from Dropbox."""
+        self.logger.info(f"Deleting '{filename}' from Dropbox")
+        self.dbx.files_delete(str(Path(filename).resolve()))
 
-        self.log.info(f"Deleting '{filename}' from dropbox")
+    def move(self, src, dest, overwrite=True):
+        """Move (rename) a file in Dropbox."""
+        self.logger.info(f"Moving '{src}' to '{dest}'")
 
-        filename = self._check_path(filename)
+        if overwrite and self.file_exists(dest):
+            self.dbx.files_delete(dest)
 
-        self.dbx.files_delete(filename)
+        self.dbx.files_copy(src, dest)
+        self.dbx.files_delete(src)
 
-    def _raw_read(self, filename):
-        """Auxiliar function for reading from dropbox"""
+    def _download(self, filename):
+        """Download a file from Dropbox."""
+        self.logger.info(f"Reading {filename=} from Dropbox")
 
-        filename = self._check_path(filename)
-
-        _, res = self.dbx.files_download(filename)
+        _, res = self.dbx.files_download(str(Path(filename).resolve()))
         res.raise_for_status()
         return res.content
 
+    def _upload(self, data, filename, as_binary=False):
+        """Upload a file to Dropbox."""
+        self.logger.info(f"Uploading {filename=} to Dropbox")
+
+        if not as_binary:
+            data = data.encode()
+
+        path = str(Path(filename).resolve())
+        with io.BytesIO(data) as stream:
+            self.dbx.files_upload(stream.read(), path, mode=WriteMode.overwrite)
+
     def read_file(self, filename, as_binary=False):
-        """
-        Reads a text file in dropbox.
+        """Read a text/binary file from Dropbox."""
+        content = self._download(filename)
+        return content if as_binary else content.decode()
 
-        Args:
-            filename:   name of the file
-        """
-
-        content = self._raw_read(filename)
-
-        with io.BytesIO(content) as stream:
-            out = stream.read()
-
-        if not as_binary:
-            out = out.decode()
-
-        return out
-
-    def write_file(self, text, filename, as_binary=False):
-        """
-        Uploads a text file in dropbox.
-
-        Args:
-            text:       text to write
-            filename:   name of the file
-        """
-
-        self.log.info(f"Exporting '{filename}' to dropbox")
-
-        if not filename.startswith("/"):
-            filename = "/" + filename
-
-        if not as_binary:
-            text = text.encode()
-
-        with io.BytesIO(text) as stream:
-            stream.seek(0)
-
-            # Write a text file
-            self.dbx.files_upload(stream.read(), filename, mode=WriteMode.overwrite)
+    def write_file(self, content, filename, as_binary=False):
+        """Write a text/binary file to Dropbox."""
+        self._upload(content, filename, as_binary)
 
     def read_yaml(self, filename):
-        """
-        Read a yaml from dropbox as an ordered dict
-
-        Args:
-            filename:   name of the yaml file
-        """
-
-        content = self._raw_read(filename)
-
-        with io.BytesIO(content) as stream:
-            return yaml.safe_load(stream)
+        """Read a YAML file from Dropbox."""
+        content = self._download(filename)
+        return yaml.safe_load(io.BytesIO(content))
 
     def write_yaml(self, data, filename):
-        """
-        Uploads a dict/ordered dict as yaml in dropbox.
-
-        Args:
-            data:       dict or dict-like info
-            filename:   name of the yaml file
-        """
-
-        self.log.info(f"Exporting '{filename}' to dropbox")
-
-        filename = self._check_path(filename)
-
+        """Write a YAML file to Dropbox."""
         with io.StringIO() as stream:
             yaml.dump(data, stream, default_flow_style=False, indent=4)
-            stream.seek(0)
+            self._upload(stream.getvalue(), filename)
 
-            self.dbx.files_upload(
-                stream.read().encode(), filename, mode=WriteMode.overwrite
-            )
+    def read_parquet(self, filename, **kwargs):
+        """Read a Parquet file into a Pandas DataFrame."""
+        return pd.read_parquet(io.BytesIO(self._download(filename)), **kwargs)
 
-    def read_parquet(self, filename, **kwa):
-        """
-        Read a parquet from dropbox as a pandas dataframe
-
-        Args:
-            filename:   name of the parquet file
-            **kwa:      keyworded arguments for the pd.read_parquet inner function
-        """
-
-        content = self._raw_read(filename)
-
-        with io.BytesIO(content) as stream:
-            return pd.read_parquet(stream, **kwa)
-
-    def read_csv(self, filename, **kwa):
-        """
-        Read a csv from dropbox as a pandas dataframe
-
-        Args:
-            filename:   name of the csv file
-            **kwa:      keyworded arguments for the pd.read_csv inner function
-        """
-
-        content = self._raw_read(filename)
-
-        with io.BytesIO(content) as stream:
-            return pd.read_csv(stream, **kwa)
-
-    def write_parquet(self, df, filename, **kwa):
-        """
-        Write a parquet to dropbox from a pandas dataframe.
-
-        Args:
-            df:         pandas dataframe
-            filename:   name of the yaml file
-            **kwa:      keyworded arguments for the pd.to_parquet inner function
-        """
-
-        self.log.info(f"Exporting '{filename}' to dropbox")
-
-        filename = self._check_path(filename)
-
+    def write_parquet(self, df, filename, **kwargs):
+        """Write a Pandas DataFrame to a Parquet file in Dropbox."""
         with io.BytesIO() as stream:
-            df.to_parquet(stream, **kwa)
-            stream.seek(0)
+            df.to_parquet(stream, **kwargs)
+            self._upload(stream.getvalue(), filename, as_binary=True)
 
-            self.dbx.files_upload(stream.getvalue(), filename, mode=WriteMode.overwrite)
+    def read_csv(self, filename, **kwargs):
+        """Read a CSV file into a Pandas DataFrame."""
+        return pd.read_csv(io.BytesIO(self._download(filename)), **kwargs)
 
-    def read_excel(self, filename, sheet_names=None, **kwa):
-        """
-        Read an excel from dropbox as a pandas dataframe
+    def write_csv(self, df, filename, **kwargs):
+        """Write a Pandas DataFrame to a CSV file in Dropbox."""
+        with io.StringIO() as stream:
+            df.to_csv(stream, **kwargs)
+            self._upload(stream.getvalue(), filename)
 
-        Args:
-            filename:       name of the excel file
-            sheet_names:    names of the sheets to read (if None read the only sheet)
-            **kwa:          keyworded arguments for the pd.read_excel inner function
-        """
-
-        content = self._raw_read(filename)
-
-        # Read one dataframe
-        if sheet_names is None:
-            with io.BytesIO(content) as stream:
-                return pd.read_excel(stream, **kwa)
-
-        # Read multiple dataframes
+    def read_excel(self, filename, sheet_name=None, **kwargs):
+        """Read an Excel file into a Pandas DataFrame."""
+        content = self._download(filename)
         with io.BytesIO(content) as stream:
-            return {x: pd.read_excel(stream, sheet_name=x, **kwa) for x in sheet_names}
+            return pd.read_excel(stream, sheet_name=sheet_name, **kwargs)
 
-    def write_excel(self, df, filename, **kwa):
-        """
-        Write an excel to dropbox from a pandas dataframe
-
-        Args:
-            filename:   name of the excel file
-            **kwa:      keyworded arguments for the df.to_excel inner function
-        """
-
-        self.log.info(f"Exporting '{filename}' to dropbox")
-
-        filename = self._check_path(filename)
-
+    def write_excel(self, df, filename, **kwargs):
+        """Write a Pandas DataFrame to an Excel file in Dropbox."""
         with io.BytesIO() as stream:
-            writer = pd.ExcelWriter(stream)
-            df.to_excel(writer, **kwa)
+            with pd.ExcelWriter(stream) as writer:
+                df.to_excel(writer, **kwargs)
+            self._upload(stream.getvalue(), filename, as_binary=True)
 
-            writer.save()
-            stream.seek(0)
-
-            self.dbx.files_upload(stream.getvalue(), filename, mode=WriteMode.overwrite)
-
-    def read_zip(self, zipfile, file=None, **kwa):
-        """
-        Reads a zip file in dropbox.
-
-        Args:
-            zipfile:    name of the file
-            file:       file to read inside the zip. If None read the first one
-            **kwa:      keyworded arguments for the zip.read function
-        """
-
-        content = self._raw_read(zipfile)
-
+    def read_zip(self, zip_filename, file_inside=None):
+        """Read a file inside a ZIP archive in Dropbox."""
+        content = self._download(zip_filename)
         with io.BytesIO(content) as stream:
-            data = ZipFile(stream)
-
-            # Get the first file
-            if file is None:
-                file = data.namelist()[0]
-
-            return data.read(file, **kwa)
+            with ZipFile(stream) as zip_file:
+                file_inside = file_inside or zip_file.namelist()[0]
+                return zip_file.read(file_inside)
